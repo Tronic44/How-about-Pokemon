34363bc1241215e69b8117bf47a0c0277a945d54
Realchicken <43414151+Realchicken@users.noreply.github.com>
2018-09-20 14:10:52 +0200

    teste_gui

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -13,6 +13,9 @@ import java.awt.event.ActionEvent;
 import java.awt.CardLayout;
 import javax.swing.JPanel;
 import javax.swing.JMenuBar;
+import javax.swing.JList;
+import javax.swing.JFormattedTextField;
+import javax.swing.JTextPane;
 
 public class MainMenu {
 
@@ -40,7 +43,7 @@ public class MainMenu {
 
 	private void openMainMenu() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 420, 581);
+		frame.setBounds(100, 100, 409, 640);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new CardLayout(0, 0));
 		
@@ -61,6 +64,14 @@ public class MainMenu {
 		JLabel lblNewLabel = new JLabel("\"Draft\"");
 		lblNewLabel.setBounds(169, 212, 46, 14);
 		panelStartDraft.add(lblNewLabel);
+		
+		JButton btnTierlist = new JButton("Tierlist");
+		btnTierlist.setBounds(63, 11, 255, 71);
+		panelStartDraft.add(btnTierlist);
+		
+		JButton btnFertig = new JButton("Fertig");
+		btnFertig.setBounds(63, 408, 255, 76);
+		panelStartDraft.add(btnFertig);
 		panelStartDraft.setVisible(false);
 		
 		JLabel lblPokemonRandomDraft = new JLabel("Pokemon Random Draft");
@@ -99,6 +110,60 @@ public class MainMenu {
 		btnExit.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnExit.setBounds(132, 373, 126, 66);
 		panelMainMenu.add(btnExit);
+		
+		JPanel panel_tierlist = new JPanel();
+		frame.getContentPane().add(panel_tierlist, "name_2032838076395");
+		panel_tierlist.setLayout(null);
+		
+		JLabel lblPokemon = new JLabel("Pokemon");
+		lblPokemon.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblPokemon.setBounds(10, 0, 73, 28);
+		panel_tierlist.add(lblPokemon);
+		
+		JList list = new JList();
+		list.setBounds(20, 39, 174, 467);
+		panel_tierlist.add(list);
+		
+		JPanel panel_draft = new JPanel();
+		frame.getContentPane().add(panel_draft, "name_2679324427935");
+		panel_draft.setLayout(null);
+		
+		JLabel lblTeam = new JLabel("Team 1");
+		lblTeam.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblTeam.setBounds(519, 11, 80, 31);
+		panel_draft.add(lblTeam);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(54, 110, 169, 98);
+		panel_draft.add(btnNewButton_1);
+		
+		JButton button = new JButton("New button");
+		button.setBounds(464, 110, 169, 98);
+		panel_draft.add(button);
+		
+		JButton button_1 = new JButton("New button");
+		button_1.setBounds(860, 110, 169, 98);
+		panel_draft.add(button_1);
+		
+		JButton button_2 = new JButton("New button");
+		button_2.setBounds(260, 280, 169, 98);
+		panel_draft.add(button_2);
+		
+		JButton button_3 = new JButton("New button");
+		button_3.setBounds(675, 280, 169, 98);
+		panel_draft.add(button_3);
+		
+		JButton button_4 = new JButton("New button");
+		button_4.setBounds(54, 441, 169, 98);
+		panel_draft.add(button_4);
+		
+		JButton button_5 = new JButton("New button");
+		button_5.setBounds(464, 441, 169, 98);
+		panel_draft.add(button_5);
+		
+		JButton button_6 = new JButton("New button");
+		button_6.setBounds(860, 441, 169, 98);
+		panel_draft.add(button_6);
 		
 		
 		JButton btnMainmenu = new JButton("MainMenu");
