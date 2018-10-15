59352fb99278296767fd249ab058e5444f9dcebe
Yannick Dreher <33758529+yannickdreherstudium@users.noreply.github.com>
2018-10-15 16:55:07 +0200

    added filtercomcobox

---
+++ src/client/FilterComboBox.java
@@ -0,0 +1,77 @@
+package client;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class FilterComboBox extends JComboBox {
+	private List<String> array;
+
+	public FilterComboBox(List<String> array) {
+		super(array.toArray());
+		this.array = array;
+		this.setEditable(true);
+		final JTextField textfield = (JTextField) this.getEditor().getEditorComponent();
+		textfield.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent ke) {
+				SwingUtilities.invokeLater(new Runnable() {
+					public void run() {
+						comboFilter(textfield.getText());
+					}
+				});
+			}
+		});
+
+	}
+
+	public FilterComboBox(String[] array) {
+		super(array);
+		List<String> arr = null;
+		for (String k : array) {
+			arr.add(k);
+		}
+		this.array = arr;
+		this.setEditable(true);
+		final JTextField textfield = (JTextField) this.getEditor().getEditorComponent();
+		textfield.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent ke) {
+				SwingUtilities.invokeLater(new Runnable() {
+					public void run() {
+						comboFilter(textfield.getText());
+					}
+				});
+			}
+		});
+
+	}
+
+	public void comboFilter(String enteredText) {
+		if (!this.isPopupVisible()) {
+			this.showPopup();
+		}
+
+		List<String> filterArray = new ArrayList<String>();
+		for (int i = 0; i < array.size(); i++) {
+			if (array.get(i).toLowerCase().contains(enteredText.toLowerCase())) {
+				filterArray.add(array.get(i));
+			}
+		}
+		if (filterArray.size() > 0) {
+			DefaultComboBoxModel model = (DefaultComboBoxModel) this.getModel();
+			model.removeAllElements();
+			for (String s : filterArray)
+				model.addElement(s);
+
+			JTextField textfield = (JTextField) this.getEditor().getEditorComponent();
+			textfield.setText(enteredText);
+		}
+	}
+
+
+}
